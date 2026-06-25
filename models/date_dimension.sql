26b77df3382a4ce5eb88fefb8b61fe4f584be0ab
Atit Wajanasomboonkul <ttteety@yahoo.com>
2026-06-25 06:03:31 +0000

    daily_weather model created

--- models/date_dimension.sql
+++ models/date_dimension.sql
@@ -3,16 +3,8 @@ WITH cte AS (
         to_timestamp(started_at) as started_at,
         date(to_timestamp(started_at)) as date_started_at,
         hour(to_timestamp(started_at)) as hour_started_at,
-        case 
-            when dayname(to_timestamp(started_at)) in ('Sat', 'Sun') then 'WEEKEND'
-            else 'BUSINESSDAY'
-        end as day_type,
-        case 
-            when month(to_timestamp(started_at)) in (12, 1, 2) then 'WINTER'
-            when month(to_timestamp(started_at)) in (3, 4, 5) then 'SRPING'
-            when month(to_timestamp(started_at)) in (6, 7, 8) then 'SUMMER'
-            else 'AUTUMN'
-        end as station_of_year,
+        {{ day_type('STARTED_AT') }} as day_type,
+        {{ get_season('STARTED_AT')}} as station_of_year,
     from {{ source('demo', 'bike') }}
     where started_at != 'started_at'
 )
